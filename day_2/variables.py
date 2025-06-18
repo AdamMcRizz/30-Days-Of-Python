8a7a39e3577597cfb7cf23d183173b9eba8419c7
Adam Gachau <adamgachao06@gmail.com>
2025-06-18 16:07:40 +0300

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,10 @@
+#Day 2- 30 days of Python
+first_name= "Adam"
+last_name='Gachau'
+full_name= first_name + last_name
+country='Kenya'
+is_married='No'
+age='94'
+
+print(type(age))
+print(type(first_name))
